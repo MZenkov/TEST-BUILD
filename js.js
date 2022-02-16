37bc880be8bb6256ca9732c258104c6e4a74fd12
M.Zenkov <nadziro@mail.ru>
2022-02-16 13:53:14 +0500

    s

--- js.js
+++ js.js
@@ -18,6 +18,7 @@ function getPost() {
             </tr>
             `
             con=con+1;
+            getPost();
         }
     })
     .catch((error)=>{
@@ -25,31 +26,32 @@ function getPost() {
     })
 }
 
-var form = document.getElementById('form')
+// var form = document.getElementById('form')
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
+// form.addEventListener('submit', function(e){
+//     e.preventDefault()
 
-    var name = document.getElementById('name').value
-    var body = document.getElementById('body').value
+//     var name = document.getElementById('name').value
+//     var body = document.getElementById('body').value
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-method: 'POST',
-body: JSON.stringify({
-title:name,
-body:body,
-}),
-headers: {
-'Content-type': 'application/json; charset=UTF-8',
-},
-})
-.then(function(response){
-return response.json()
-})
-.then(function(data){
-console.log(data)
-var results = document.getElementById('results')
-results.innerHTML = `<p>Пользователь ${data.title}</p>
-<p>Оставил запись: ${data.body}</p>`
-})
-})
+//     fetch('https://jsonplaceholder.typicode.com/posts', {
+// method: 'POST',
+// body: JSON.stringify({
+// title:name,
+// body:body,
+// }),
+// headers: {
+// 'Content-type': 'application/json; charset=UTF-8',
+// },
+// })
+// .then(function(response){
+// return response.json()
+// })
+// .then(function(data){
+// console.log(data)
+// var results = document.getElementById('results')
+// results.innerHTML = `<p>Пользователь ${data.title}</p>
+// <p>Оставил запись: ${data.body}</p>`
+// })
+// })
+getPost();
